838e8c1f51c61674ff5cde8cb47500877ffcb71d
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-05-22 15:32:46 +0200

    Add logic for fetch rate

--- src/components/Rate/Rate.jsx
+++ src/components/Rate/Rate.jsx
@@ -1,17 +1,27 @@
+import { useEffect, useState } from 'react';
 import './style.css';
 
-const currencies = {
-  USD: { CZK: 23.823 },
-  EUR: { CZK: 24.74 },
-  GBP: { CZK: 29.067 },
-};
+//2. Upravte komponentu `Rate` tak, aby si z API stáhla správný kurz pro měnu, kterou dostane v prop `from`. Zařiďte, aby se správná data stáhla kdykoliv, když se změní hodnota uvnitř *prop* `from`. K tomu budete potřebovat `useEffect` se závislostí na *prop* `from`.
 
 export const Rate = ({ from }) => {
+  const [rate, setRate] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://api.frankfurter.app/latest?from=${from}&to=CZK`);
+      const json = await response.json();
+      const exRate = json.rates.CZK;
+      setRate(exRate);
+    };
+    fetchData();
+  }),
+    [from];
+
   return (
     <div className="rate">
       <div className="rate__currency">1 {from}</div>
       <div>=</div>
-      <div className="rate__value">{currencies[from].CZK} CZK</div>
+      <div className="rate__value">{rate} CZK</div>
     </div>
   );
 };
