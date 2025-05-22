073026461d5aa6aaa6c326897339fd597629e8e8
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-05-22 15:34:31 +0200

    I didn’t save the component before...

--- src/components/Rate/Rate.jsx
+++ src/components/Rate/Rate.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 import './style.css';
 
-//2. Upravte komponentu `Rate` tak, aby si z API stáhla správný kurz pro měnu, kterou dostane v prop `from`. Zařiďte, aby se správná data stáhla kdykoliv, když se změní hodnota uvnitř *prop* `from`. K tomu budete potřebovat `useEffect` se závislostí na *prop* `from`.
-
 export const Rate = ({ from }) => {
   const [rate, setRate] = useState(null);
 
